c62efcb46c0a8b5af8af415a9320fdbf62b965f4
yarivvv <yarivvv@0e8c9efb-304e-0410-a30b-3d67abe68fb5>
2008-06-06 04:24:02 +0000

    tweaked db settings

--- trunk/twoorl/src/twoorl_app.hrl
+++ trunk/twoorl/src/twoorl_app.hrl
@@ -2,5 +2,6 @@
 -define(DB_USERNAME, "root").
 -define(DB_PASSWORD, "password").
 -define(DB_DATABASE, "twoorl").
+-define(DB_POOL_SIZE, 50).
 %-define(APP_PATH, "/home/yariv/apps/twoorl/trunk/twoorl").
 -define(APP_PATH, "/Users/yariv/Documents/dev/twoorl/trunk/twoorl").
